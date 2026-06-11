c302c032e4be82f87aae03aa549b4684d29dd0ce
7arzz <7arzz404@gmail.com>
2026-06-11 19:22:29 +0700

    responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/LogoFinal.jpg" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
